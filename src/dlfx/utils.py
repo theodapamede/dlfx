b2102233e9805b569ee6e32d2efc985bfa8b1cc5
Theo Dapamede <theo.dapamede@emory.edu>
2026-01-12 11:46:57 -0500

    default author in stamp_notebook

--- src/dlfx/utils.py
+++ src/dlfx/utils.py
@@ -1,16 +1,17 @@
 import os
 import datetime
+import getpass
 from IPython.display import display, Markdown
 
-def stamp_notebook(created_date, author="", notebook_path=None):
+def stamp_notebook(created_date, notebook_path=None, author=getpass.getuser()):
     """
     Displays a formatted info block to track notebook creation and modification.
     Distinct from DICOM/Image metadata.
     
     Args:
         created_date (str): The date the notebook was created (e.g., "2025-08-15").
-        author (str): Researcher name.
         notebook_path (str): The filename of the .ipynb file to pull 'Last Modified' stats.
+        author (str): Author name.
     """
     info_lines = [
         "**Notebook Session Info:**",
@@ -25,7 +26,7 @@ def stamp_notebook(created_date, author="", notebook_path=None):
     else:
         info_lines.append("* **Last Modified:** (Run save to update / path not found)")
         
-    info_lines.append(f"* **Researcher:** {author}")
+    info_lines.append(f"* **Author:** {author}")
 
     # Render as a clean Markdown block
     display(Markdown("---"))
