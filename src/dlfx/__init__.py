71cdcfecd1ff4eda26c9c883863e844abb1b91e8
Theo Dapamede <theo.dapamede@emory.edu>
2026-01-12 11:26:51 -0500

    added utils.py

--- src/dlfx/__init__.py
+++ src/dlfx/__init__.py
@@ -1,6 +1,7 @@
 from importlib.metadata import version, PackageNotFoundError
 
 from .dicom import flatten_dicom_dataset, extract_index
+from .utils import stamp_notebook
 
 try:
     __version__ = version(__name__)
